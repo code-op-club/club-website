6bbef79f7a99678636dabacc9f602462175a364a
Franklin Liu <fezhead@parabolas.xyz>
2022-10-01 19:04:50 -0700

    auth0 provider stuff

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import BrowserRouter from 'react'
+import Auth0Provider from '@auth0/auth0-react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Auth0Provider>
+        <App />
+      </Auth0Provider>
+    </BrowserRouter>
   </React.StrictMode>
 )
