12be34eb1d519886c1d914f42bacc26fcb96405b
Franklin Liu <fezhead@parabolas.xyz>
2022-10-01 19:30:00 -0700

    add auth0withhistory component

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import BrowserRouter from 'react'
-import Auth0Provider from '@auth0/auth0-react'
+import Auth0ProviderWithHistory from './auth/auth0-provider-with-history.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0Provider>
+      <Auth0ProviderWithHistory>
         <App />
-      </Auth0Provider>
+      </Auth0ProviderWithHistory>
     </BrowserRouter>
   </React.StrictMode>
 )
+
