6858ba48cf33bee86fc49d3f0a97bdcd0cb79ccd
James Dowdell <james@ptowngames.com>
2022-10-08 16:55:47 -0700

    Vite requires JSX to be in .jsx files

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import BrowserRouter from 'react'
-import Auth0ProviderWithHistory from './auth/auth0-provider-with-history.js'
+import Auth0ProviderWithHistory from './auth/auth0-provider-with-history.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
